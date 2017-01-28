70587fa4aaf893657058624f6092a0da5397e220
AnkithaP <ankitha.parameswara@gmail.com>
2017-01-28 14:33:30 +0530

    Toolbar added GetStarted activity added User Login screen added

--- app/src/main/java/com/ankitha/gullyfood/gullyfoodlayouts/UserLogin.java
+++ app/src/main/java/com/ankitha/gullyfood/gullyfoodlayouts/UserLogin.java
@@ -2,6 +2,7 @@ package com.ankitha.gullyfood.gullyfoodlayouts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class UserLogin extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class UserLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_login);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 }
